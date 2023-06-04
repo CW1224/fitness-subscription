c084557a119bb3a0d89a83363c406f7f9a8e93b3
CW1224 <xixiwang1224@gmail.com>
2023-06-04 10:46:53 +0000

    Deployment commit part 1

--- favourites/views.py
+++ favourites/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
 def view_favourites(request):
     """ A view that renders the bag contents"""
 
+    favorite_products = Product.objects.filter(users_favorite=request.user)
+
+    context = {
+        'favorite_products': favorite_products,
+    }
+
     return render(request, 'favourites/favourites.html')
+
+def add_to_favorites(request, item_id):
+    """ Add product to the user's favorites """
+    product = get_object_or_404(Product, id=item_id)
+
+    if product.users_favorite.filter(id=request.user.id).exists():
+        product.users_favorite.remove(request.user)
+        messages.success(request,
+                         f'{product.name} has been \
+                         removed from your favorites.')
+    else:
+        product.users_favorite.add(request.user)
+        messages.success(request,
+                         f'{product.name} has been \
+                         added to your favorites.')
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
