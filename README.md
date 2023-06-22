b9e614fa56372ff4043a9b29fca6e11ecf6025d7
CW1224 <xixiwang1224@gmail.com>
2023-06-22 20:53:17 +0000

    Modified header to suit my site

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 All the code that are in this project comes from Boutique Ado. I am using this code as a template for my project itself.
+
+Image by <a href="https://www.freepik.com/free-vector/flat-design-no-photo-sign_23101739.htm#query=no%20camera&position=4&from_view=keyword&track=ais">Freepik</a>
--- favourites/urls.py
+++ favourites/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.view_favourites, name='view_favourites'),
     path('add_favourite/<item_id>/', views.add_to_favorites, name='add_to_favorites'),
-    #path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
-    #path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
