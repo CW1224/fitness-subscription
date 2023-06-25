97f4a52e4f202a690823cab99fc021fa6386a4f1
CW1224 <xixiwang1224@gmail.com>
2023-06-25 10:19:53 +0000

    Added in page navigation in home page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def index(request):
     """ A view to return to index page"""
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """ A view to return to index page"""
+
+    return render(request, 'home/about.html')
+
+def contact(request):
+    """ A view to return to index page"""
+
+    return render(request, 'home/contact.html')
