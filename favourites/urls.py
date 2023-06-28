5d89c5b7780d320ececbf29b3a84bfc2dc8734eb
CW1224 <xixiwang1224@gmail.com>
2023-06-28 21:27:39 +0000

    Added in navigation to mobile website

--- favourites/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_favourites, name='view_favourites'),
-    path('add_favourite/<item_id>/', views.add_to_favorites, name='add_to_favorites'),
-]
